1e123f861081bc272b3247b12c4e7b40bb7c3d74
deanrobin333 <deanrobin777@gmail.com>
2023-03-16 23:19:29 +0000

    hope

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - We are printing into standard error
+* main - We are into standard error
 * Return: We shal return 0
 */
-
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,
+		2015-10-19\n");
 	return (1);
 }
