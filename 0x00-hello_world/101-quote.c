d3897ced3184f4cd7501cb4ca48d9a3201eabd29
deanrobin333 <deanrobin777@gmail.com>
2023-03-16 23:07:53 +0000

    8-stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+* main - We are printing into standard error
+* Return: We shal return 0
+*/
+
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	return (1);
+}
