4bbb73423959fea28ae848dd1efd1203324c7666
deanrobin333 <deanrobin777@gmail.com>
2023-03-16 21:44:35 +0000

    5print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Printing using prinf
+* Return: This will return 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of at,\n");
+	return (0);
+}
