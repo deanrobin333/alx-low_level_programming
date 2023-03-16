933ccc62e2b505633e853c8d84515a05b81169b3
deanrobin333 <deanrobin777@gmail.com>
2023-03-16 23:33:04 +0000

    hope

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,\
-	2015-10-19\n");
+	fprintf(stderr, "[stderr] and that piece of art is useful\" - Dora Korpar,\
+	2015-10-19 %s\n", "Anything");
 	return (1);
 }
