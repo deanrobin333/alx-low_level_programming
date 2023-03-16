989e3736bfd45ec0d0840de8ac940d3dc3ab2e93
deanrobin333 <deanrobin777@gmail.com>
2023-03-16 21:25:21 +0000

    4-first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Program that prints using only put.
+* Return: This returns 0.
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
