c5506444d6a96b769e814683011b000852baf624
deanrobin333 <deanrobin777@gmail.com>
2023-03-17 00:29:21 +0000

    hopefull

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 /**
 * main - We are into standard error
 * Return: We shal return 1
 */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar,\
-	2015-10-19\n";
-	
-	size_t len = strlen(message);
-	fwrite(message, sizeof(char), len, stderr);
-	
+	fputs("and that piece of art is useful\" - Dora Korpar, \
+	2015-10-19\n", stdout);
+
 	return (1);
 }
