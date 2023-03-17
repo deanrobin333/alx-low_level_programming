d335c9c904709a040c2c73d9c1893e35db3e093e
deanrobin333 <deanrobin777@gmail.com>
2023-03-17 21:50:12 +0000

    0-positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - if else code
+* Return: always zero
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+
+	return (0);
+}
