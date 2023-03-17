ea0ddd622e5071becf8586b96e6c66d856ca3b17
deanrobin333 <deanrobin777@gmail.com>
2023-03-17 23:11:33 +0000

    2 putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Print Alphabet
+* Return: - Always 0
+*/
+
+int main(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
