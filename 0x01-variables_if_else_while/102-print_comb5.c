fc36c4681c881ab2edb318e1c2269cdad2e6ab41
deanrobin333 <deanrobin777@gmail.com>
2023-03-18 02:11:53 +0000

    102 combo5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+* main - two loops
+* Return: 0
+*/
+
+int main(void)
+{
+	int s;
+	int b;
+	int S;
+	int B;
+
+	for (s = 0; s <= 9; s++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (S = 0; S <= 9; S++)
+			{
+				for (B = 0; B <= 9; B++)
+				{
+					putchar(s + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(S + '0');
+					putchar(B + '0');
+					if (s == 9 && b == 8 && S == 9 && B == 9)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
