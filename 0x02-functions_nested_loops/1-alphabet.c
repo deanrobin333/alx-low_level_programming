db9672e98302d275ccd1c072dfb64e3100e60bcf
deanrobin333 <deanrobin777@gmail.com>
2023-03-21 21:47:53 +0000

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ * main - printing alphabet
+ * Return: always true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+}
