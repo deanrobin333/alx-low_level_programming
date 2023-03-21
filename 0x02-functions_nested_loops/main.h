07790c0d495980f5b25a2ece8ecae7b4ea1b6187
deanrobin333 <deanrobin777@gmail.com>
2023-03-21 22:31:45 +0000

    1-moved contents to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,32 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c);
+#include <unistd.h>
 
-void print_alphabet(void);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+
+
+/**
+ * print_alphabet - prints the letters of the alphabet
+ */
+
+void print_alphabet(void)
+{
+        int i;
+
+        for (i = 'a'; i < 'z'; i++)
+        {
+                _putchar(i);
+        }
+}
+
