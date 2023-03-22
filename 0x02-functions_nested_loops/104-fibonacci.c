e74639dbddf58686b74b5fb3e1b665106c858565
deanrobin333 <deanrobin777@gmail.com>
2023-03-22 23:00:49 +0000

    16 -104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - printing first 98 fibonacci
+ * no hard code
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long first = 1, second = 2, next;
+
+	for (i = 1; i <=98; i++)
+	{
+		if (i <= 1)
+		{
+			next = i + 1;
+		}
+		else
+		{
+			next = first + second;
+			first = second;
+			second = next;
+		}
+		if (i < 98)
+		{
+			printf("%lu, ", next);
+		}
+		else
+		{
+			printf("%lu", next);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
+
