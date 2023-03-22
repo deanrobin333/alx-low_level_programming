ee4a09fddeda582793d2aa91caa648fc2f1d8564
deanrobin333 <deanrobin777@gmail.com>
2023-03-22 21:45:49 +0000

    13 - 101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - sum of multiples of 3 or 5 below 1024
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int s;
+	for (i = 0; i <= 1024; i++)
+	{
+		if((i % 3 == 0) || (i % 5 == 0))
+		{
+			s += i;
+		}
+	}
+	printf("%d\n",s);
+	return (0);
+}
