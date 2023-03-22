6f229d7db1306022fd29d76a98ed45ed32a87dd9
deanrobin333 <deanrobin777@gmail.com>
2023-03-22 23:05:38 +0000

    16 -104-fibonacci - 1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	int i;
 	unsigned long first = 1, second = 2, next;
 
-	for (i = 1; i <=98; i++)
+	for (i = 0; i < 98; i++)
 	{
 		if (i <= 1)
 		{
@@ -22,7 +22,7 @@ int main(void)
 			first = second;
 			second = next;
 		}
-		if (i < 98)
+		if (i < 97)
 		{
 			printf("%lu, ", next);
 		}
