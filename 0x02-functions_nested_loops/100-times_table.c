886e74e5704cc0a9b9ab4bb571cd6bd220ef2256
deanrobin333 <deanrobin777@gmail.com>
2023-03-22 21:07:17 +0000

    12 betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,37 +8,33 @@
 void print_times_table(int n)
 {
 	int i;
-	int j;
-	int k;
+	int j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				k = i * j;
+				k = j * i;
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} 
-				else if (k >= 10 && k < 100)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -48,7 +44,7 @@ void print_times_table(int n)
 				}
 			}
 			_putchar('\n');
-
 		}
 	}
+
 }
