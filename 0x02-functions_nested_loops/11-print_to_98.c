d53f54a71a4d117d280ca236ecb6b47f70c99202
deanrobin333 <deanrobin777@gmail.com>
2023-03-22 03:22:29 +0000

    11-1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,33 +2,30 @@
 #include "main.h"
 
 /**
- * print_to_98 - printing numbers from n to 98
- * @n: integers
- * Return: 0
- */
+* print_to_98 - printing numbers from n to 98
+* @n: integers
+* Return: 0
+*/
 
 void print_to_98(int n)
 {
-		int i, j;
-
-		if (n <= 98)
+	while (n <= 98)
+	{
+		if (n >= 0 && n <= 9)
 		{
-			for (i = n; i <= 98; i++)
-			{
-				if (i != 98)
-					_putchar(i + '0') ;
-				else if (i == 98)
-					_putchar(i + '0');
-			}
-		} else if (n >= 98)
-		{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					_putchar(j + '0');
-				else if (j == 98)
-					_putchar(j + '0');
-			}
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
 		}
+		else
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar(' ');
+		n++;
 	}
-
+	_putchar('\n');
+}
