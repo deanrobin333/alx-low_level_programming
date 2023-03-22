66b16ef200fa87406d905ce77e5602220c9759a4
deanrobin333 <deanrobin777@gmail.com>
2023-03-22 00:19:35 +0000

    2 - output -3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,12 +14,9 @@ void print_alphabet_x10(void)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
-			if(j != '\0')
-			{
-			putchar('\n');
-			}
-			_putchar(j);
+			_putchar (j);
 		}
+	putchar('\n');
 	}
 	putchar('\n');
 }
