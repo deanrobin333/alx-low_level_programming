ca478c3925d37314938fd72892a0ec6152244cd2
deanrobin333 <deanrobin777@gmail.com>
2023-03-22 22:00:49 +0000

    14 - 102 Fibonnaci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    unsigned long long first = 1, second = 2, next;
+
+    for (i = 0; i < 50; i++) {
+        if (i <= 1) {
+            next = i + 1;
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%llu, ", next);
+    }
+	printf("\n");
+
+    return 0;
+}
+
