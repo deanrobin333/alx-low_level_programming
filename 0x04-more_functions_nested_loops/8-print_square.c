ea97c4bafdd1dd0ee1eae2b214f6d76c06032d5a
deanrobin333 <deanrobin777@gmail.com>
2023-03-24 01:32:56 +0000

    8-print squares loop issue

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,19 +12,19 @@ void print_square(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		if (n > 0)
 		{
-			if (n > 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar('#');
+				putchar('#');
 			}
-			else
-			{
-				putchar('\n');
-			}
+			putchar('\n');
 		}
-		_putchar('\n');
+		else
+		{
+			_putchar('\n');
+		}
 	}
 }
