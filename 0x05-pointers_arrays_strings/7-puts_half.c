9dca716f6b115b2a6567f656ee32dad3da899b4e
deanrobin333 <deanrobin777@gmail.com>
2023-03-27 10:48:57 +0000

    7-puts half lorem ipsum

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@
 void puts_half(char *str)
 {
 	int i;
-	int length;
+	long length;
 
 	for (length = 0; str[length] != '\0'; length++)
 	{
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	}
 	if (length % 2 == 0)
 	{
-		for (i = (length / 2); i <= length; i++)
+		for (i = (length / 2); i < length; i++)
 		{
 			_putchar(str[i]);
 		}
