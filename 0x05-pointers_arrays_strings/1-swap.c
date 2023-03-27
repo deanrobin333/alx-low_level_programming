d1d306ec1ae35f7255cf1682ddde25d43678a329
deanrobin333 <deanrobin777@gmail.com>
2023-03-27 04:53:58 +0000

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - a pointer function
+ * @a: takes first pointer as input.
+ * @b: takes 2nd pointer as input
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
