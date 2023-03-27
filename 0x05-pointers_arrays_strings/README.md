5a97db11947244b163724962c8b1b9c8276b2160
deanrobin333 <deanrobin777@gmail.com>
2023-03-27 04:26:17 +0000

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+#Pointers and Arrays
+Started doing POinters and Arrays
+Date Mon 27th March 2023
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#include <unistd.h>
+/**
+ * custom header
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
