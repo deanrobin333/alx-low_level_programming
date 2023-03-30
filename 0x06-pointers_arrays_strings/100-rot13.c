92131ca455fa8ef995082ad804edc3d89dcc8a86
deanrobin333 <deanrobin777@gmail.com>
2023-03-30 08:06:51 +0000

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes string using rot13 - rotate by 13 places
+ * @s: pointer to string to be encoded
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
