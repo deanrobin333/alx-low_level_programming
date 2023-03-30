03cf6b704cd7b96a1f8e7f7f18197270e4235897
deanrobin333 <deanrobin777@gmail.com>
2023-03-30 06:18:47 +0000

    1-strncat.c - two loops

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,15 +16,18 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		;
+		i++;
 	}
-	for (j = 0; j < n && (src[j] != '\0') ; j++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
+		i++;
+		j++;
 	}
-
-	dest[strlen(dest)] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
