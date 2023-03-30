4199d068b0d19b97cb61c08ae84a2cdd46b2d2a2
deanrobin333 <deanrobin777@gmail.com>
2023-03-30 03:11:51 +0000

    1-strncat.c - First try

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenating 2 strings,
+ * up to the nth character of second string.
+ * @dest: First string
+ * @src: Source of 2nd string to be added to dest
+ * @n: number of characters of src to be added to dest.
+ * Return: Concatenating string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *joined = dest;
+	int i;
+	int len = strlen(src);
+
+	if (n < len + 1)
+	{
+		for (i = 0; src[i] < src[n] ; i++)
+		{
+			dest[strlen(dest)] += src[i];
+		}
+	}
+	else
+	{
+		for (i = 0; src[i] != '\0'; i++)
+		{
+			dest[strlen(dest)] += src[i];
+		}
+	}
+	dest[strlen(dest)] = '\0';
+
+	return (joined);
+}
