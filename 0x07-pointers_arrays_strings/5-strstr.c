7ad3a90f2d99c1000cbccbdcea7766b6e01b476d
deanrobin333 <deanrobin777@gmail.com>
2023-04-03 09:54:36 +0000

    5-strstr.c-1

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,33 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int j;
 	int i;
+	int j;
+	int k;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		if (haystack[i] == needle[0])
 		{
-			if (haystack[i] == needle[j])
+			k = i;
+			for (j = 0; needle[j] != '\0'; j++, k++)
 			{
-				return (needle);
+				if (haystack[k] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
 			}
 		}
 	}
+
 	return (NULL);
 }
