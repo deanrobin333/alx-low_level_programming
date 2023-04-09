35b5a0c2f49f704ce13a59ed0fbb2fa885215408
deanrobin333 <deanrobin777@gmail.com>
2023-04-09 14:20:07 +0000

    3-mul.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,15 +6,10 @@
  * Return: always 0.
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	for (i = 0; i < argc; i++)
-	{
-		;
-	}
 	if (*argv[0] != '\0')
 	{
-
 		printf("%d\n",argc);
 	}
 	return (0);
