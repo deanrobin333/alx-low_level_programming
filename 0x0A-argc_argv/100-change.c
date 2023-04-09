46bc3e587480bccebe984abf3ad42833e07f0125
deanrobin333 <deanrobin777@gmail.com>
2023-04-09 15:53:14 +0000

    100-change.c -3

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,35 +7,35 @@
  * @argv: array containing arguments
  * Return: always 0, 1 for error.
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-	int cents[] = {25, 10, 5, 2, 1};
-	int change;
-	int argument;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	change = 0;
-	argument = atoi(argv[1]);
 
-	if (argument < 0)
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for (i = 0; i < 5 && argument >= 0; i++)
+
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		while (argument >= cents[i])
+		while (num >= coins[j])
 		{
-			change++;
-			argument -= cents[i];
+			result++;
+			num -= coins[j];
 		}
 	}
-	printf("%d\n", change);
+
+	printf("%d\n", result);
 	return (0);
 }
