c7a0bbaefc49d0711e016ca4b573f1c506f07584
deanrobin333 <deanrobin777@gmail.com>
2023-04-09 12:19:13 +0000

    libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _isupper - Check for upper characters
+ * @c: character to be checked
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+#Using Libraries
+Started project on static libraries
+on Thursday 6th April 2023
