1763c8a47775c5889f5826fff21bfcc3aabdffda
deanrobin333 <deanrobin777@gmail.com>
2023-04-09 14:03:16 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 /**
- * main - using argv to get name of a program
+ * main - using argv to print all arguments
  * @argc: size of array
  * @argv: array containing commands
  * Return: always 0.
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	int i;
+
 	for (i = 0; i < argc; i++)
 	{
-		;
-	}
-	if (*argv[0] != '\0')
-	{
-
-		printf("%d\n",argc);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
