e07f837b1d2f16c266daebbbf07f3e14ecc85910
deanrobin333 <deanrobin777@gmail.com>
2023-04-11 16:05:26 +0000

    2-alloc_grid.c - malloc-free -4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,29 +11,41 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **array;
+	int **two_array;
 	int i, j;
 
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
-	array = malloc(height * sizeof(int *));
-	if (array == NULL)
+	}
+
+	two_array = malloc(height * sizeof(int *));
+	if (two_array == NULL)
+	{
 		return (NULL);
+	}
+
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(width * sizeof(int));
-		if (array[i] == NULL)
+		two_array[i] = malloc(width * sizeof(int));
+		if (two_array[i] == NULL)
 		{
 			for (; i >= 0; i++)
-				free(array[i]);
-			free(array);
+			{
+				free(two_array[i]);
+			}
+			free(two_array);
 			return (NULL);
 		}
 	}
 	for (i = 0; i < height; i++)
 	{
+
 		for (j = 0; j < width; j++)
-			array[i][j] = 0;
+		{
+			two_array[i][j] = 0;
+		}
 	}
-	return (array);
+
+	return (two_array);
 }
