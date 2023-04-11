e28d7b664006ca547d1a3dca83ed43c62e748479
deanrobin333 <deanrobin777@gmail.com>
2023-04-11 13:46:46 +0000

    1-strdup.c - size of

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * _strdup - returns a pointer to a newly allocated space in memory
@@ -13,8 +14,9 @@ char *_strdup(char *str)
 {
 	char *copied;
 	int i;
+	int length = strlen(str);
 
-	copied = malloc(sizeof(str));
+	copied = malloc((length * sizeof(char)) + 1);
 
 	if (copied == NULL)
 	{
