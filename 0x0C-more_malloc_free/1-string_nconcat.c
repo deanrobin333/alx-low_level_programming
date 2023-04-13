8bb47312b62dc928f3590c255c44462a24d029dc
deanrobin333 <deanrobin777@gmail.com>
2023-04-13 22:34:47 +0000

    1-string_nconcat.c - malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,34 +22,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	length = strlen(s1) + strlen(s2);
+	if (n >= strlen(s2))
+		length = strlen(s1) + strlen(s2);
+	else
+		length = strlen(s1) + n;
 	joined = malloc((length * sizeof(char)) + 1);
 
 	if (joined == NULL)
-	{
 		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		joined[i] += s1[i];
+	}
+	if (n >= strlen(s2))
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			joined[strlen(joined)] += s2[i];
+		}
 	}
 	else
 	{
-		for (i = 0; s1[i] != '\0'; i++)
+		for (i = 0; i < j; i++)
 		{
-			joined[i] += s1[i];
+			joined[strlen(joined)] += s2[i];
 		}
-		if (n >= strlen(s2))
-		{
-			for (i = 0; s2[i] != '\0'; i++)
-			{
-				joined[strlen(joined)] += s2[i];
-			}
-		}
-		else
-		{
-			for (i = 0; i < j; i++)
-			{
-				joined[strlen(joined)] += s2[i];
-			}
 
-		}
 	}
 	joined[strlen(joined)] = '\0';
 	return (joined);
