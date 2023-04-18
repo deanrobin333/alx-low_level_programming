cafea90947b0f4934f6a3da44eae4f0ac1967db6
deanrobin333 <deanrobin777@gmail.com>
2023-04-18 02:07:44 +0000

    5-free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for dog
+ * @d: struct dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,11 +15,12 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * dog_t - new name of struct dog
- */
 
 typedef struct dog dog_t;
 
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 #endif
