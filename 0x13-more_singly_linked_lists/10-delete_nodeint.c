1e5234036e88b2cc33578638873d22f31a179b41
deanrobin333 <deanrobin777@gmail.com>
2023-05-02 02:54:53 +0000

    10-delete_nodeint.c - return -1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < index - 1; i++)
 	{
-		if (current != NULL || (current->next) != NULL)
+		if (current == NULL || (current->next) == NULL)
 			return (-1);
 		current = current->next;
 	}
