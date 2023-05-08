6e14b2f5dde4ecbcd95856ab3f89fcc1bd43c5a6
deanrobin333 <deanrobin777@gmail.com>
2023-05-08 15:41:15 +0000

    1-create_file.c - null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,9 @@ int create_file(const char *filename, char *text_content)
 	int write_value;
 	int length = 0;
 
-	if (!filename)
+	if (filename)
 		return (-1);
-	if (!text_content)
+	if (text_content)
 	{
 		while (text_content[length])
 			length++;
