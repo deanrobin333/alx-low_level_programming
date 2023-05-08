7246a6a3d1c25308495bc63ba5456513182ddd09
deanrobin333 <deanrobin777@gmail.com>
2023-05-08 16:25:25 +0000

    3-cp.c - null

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ char *create_buffer(char *file)
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
-	if (!buffer)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", file);
