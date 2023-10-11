179ff1463ed47c111605ebf417c19143affa1fdd
deanrobin333 <deanrobin777@gmail.com>
2023-10-11 15:18:29 +0000

    x105-jump_list.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,24 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * 12. Jump search in a singly linked list -- 105-jump_list.c
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+
 /* Linear search -- 0-linear.c */
 int linear_search(int *array, size_t size, int value);
 
@@ -27,5 +45,8 @@ int _binary_search(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 /* helper function */
 int recursive_search(int *array, size_t size, int value);
+
+/* 12. Jump search in a singly linked list -- 105-jump_list.c */
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
