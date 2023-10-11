69deb8748765df35feb11bcfe6a178f8d0d6d382
deanrobin333 <deanrobin777@gmail.com>
2023-10-11 12:59:10 +0000

    search_algos.h, 0-linear.c

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _search_algos_h_
+#define _search_algos_h_
+#include <stdio.h>
+#include <stddef.h>
+
+/* Linear search -- 0-linear.c */
+int linear_search(int *array, size_t size, int value);
+
+#endif
