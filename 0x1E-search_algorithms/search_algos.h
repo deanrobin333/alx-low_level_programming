4abab6695ee533e4095a2a436f20d49f3a0e47be
deanrobin333 <deanrobin777@gmail.com>
2023-10-11 13:15:42 +0000

    1-binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,10 @@
 /* Linear search -- 0-linear.c */
 int linear_search(int *array, size_t size, int value);
 
+/* Binary search -- 1-binaryc.c */
+int binary_search(int *array, size_t size, int value);
+/* helper functions */
+int binary_search_helper(int *array, size_t l, size_t r, int value);
+void print_array(int *array, size_t l, size_t r);
+
 #endif
