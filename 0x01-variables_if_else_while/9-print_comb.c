ed75f15a10bf456ed94f1161acb701d4887f65ef
deanrobin333 <deanrobin777@gmail.com>
2023-12-04 23:53:56 +0300

    9-print_comb.c added new line char - 'cat -e' in expected output was confusing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,10 +14,12 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
-		if (i != 9)
-			putchar(',');
+		if (i == 9)
+			break;
+		putchar(',');
 		putchar(' ');
 	}
+	putchar('\n');
 
 	return (0);
 }
