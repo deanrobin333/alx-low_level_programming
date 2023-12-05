83d70678476587af556acfee89133d8d5c8d9bfb
deanrobin333 <deanrobin777@gmail.com>
2023-12-05 03:51:13 +0300

    0-putchar.c, confused \n with \0 for null character

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@ int main(void)
 	char string [] = "_putchar";
 	int i;
 	
-	for (i = 0; i < (int)sizeof(string); i++)
+	for (i = 0; string[i] != '\0'; i++)
 		_putchar(string[i]);
 	_putchar('\n');
 	return (0);
