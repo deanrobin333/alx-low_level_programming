6ec027a34aba0801311ecc0320327b295c689dbd
deanrobin333 <deanrobin777@gmail.com>
2023-12-06 22:19:22 +0300

    2-largest_number.c change >  and initialized largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,17 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = 0;
 
 	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b && c > a)
 	{
 		largest = c;
 	}
