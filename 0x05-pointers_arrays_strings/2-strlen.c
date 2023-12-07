f82fffd375565698d4f4b16067d1ac3a7299c731
deanrobin333 <deanrobin777@gmail.com>
2023-12-07 21:08:19 +0300

    2-strlen.c - incremented length instead of making lenght to i

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,10 @@
 
 int _strlen(char *s)
 {
-	int i, length;
+	int i, length = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		length = i + 1;
+	for (i = 0; s[i] != '\0'; ++i)
+		length++;
 
 	return (length);
 }
