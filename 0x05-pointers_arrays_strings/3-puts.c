1e25e3181bbc43cc19f34f85d4cab9e0b65f8c2f
deanrobin333 <deanrobin777@gmail.com>
2023-12-07 21:35:54 +0300

    3-puts.c - updated comments

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,8 +10,13 @@
 void _puts(char *str)
 {
 	int i = 0;
+	/**
+	 *  "*(str + i)" , is the same as "str[i]"
+	 *  so for while, we used the later version
+	 *  for _putchar we used the earlier version.
+	 */
 
-	while (*(str + i) != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(*(str + i));
 		i++;
