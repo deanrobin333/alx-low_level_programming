a7b40003837a0f357ae91209374f6e7e3925f571
deanrobin333 <deanrobin777@gmail.com>
2023-12-07 20:51:22 +0300

    1-swap.c - explanation comment

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,7 +10,15 @@
 
 void swap_int(int *a, int *b)
 {
+	/*
+	 * variable to hold a;
+	 * otherwise if we change a to b
+	 * we wont be able to assign b to a
+	 * unless we stored it somewhere first
+	 * before reassigning a
+	 */
 	int val_a = *a;
+
 	*a = *b;
 	*b = val_a;
 }
