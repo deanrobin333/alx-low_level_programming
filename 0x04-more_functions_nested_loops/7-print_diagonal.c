189e61286aa9e729e49833a183261c7f179890e5
deanrobin333 <deanrobin777@gmail.com>
2023-12-07 04:04:21 +0300

    7-print_diagonal.c - simplified, used ++ only

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,9 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (i = n; i >= 1; i--)
+	for (i = 1; i <= n; i++)
 	{
-		for (j = i; j < n; j++)
+		for (j = 1; j < i; j++)
 			_putchar(' ');
 		_putchar('\\');
 		_putchar('\n');
