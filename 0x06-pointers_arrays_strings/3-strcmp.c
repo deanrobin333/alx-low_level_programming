c34650366ac8ff89bcd6df32809d704fb9d0bcea
deanrobin333 <deanrobin777@gmail.com>
2023-12-09 02:11:50 +0300

    3-strcmp.c understood - returns differnce of characters

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,26 +5,29 @@
  * @s1: first string
  * @s2: second string
  *
- * Return:	0 if same bytes
- *			-15, if s1 has less bytes than s2
- *			15, if s1 has more bytes than s2
+ * Return: difference of first differing character
+ *
+ *		The function iterates through the strings character by character.
+ *		If, at any point, it finds a pair of characters that are not equal,
+ *		it stops the iteration and returns the difference between
+ *		the ASCII values of those characters
+ *
+ *		if The first differing characters are 'H' and 'W',
+ *		The ASCII value of 'H' is 72, and the ASCII value of 'W' is 87.
+ *		So, 72 - 87 gives -15, and that's the result returned by _strcmp.
+ *
+ *		if there are no differing characters by the time iteration is done
+ *		it retuns 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int len_s1, len_s2, result = 0;
-
-	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
-		;
-	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
-		;
-
-	if (len_s1 == len_s2)
-		result = 0;
-	else if (len_s1 < len_s2)
-		result = -15;
-	else
-		result = 15;
-
-	return (result);
+	int i;
+	
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
