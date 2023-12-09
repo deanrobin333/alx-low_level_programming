94c65a6faf6c4f23efdbb0ce7c0da8efb2cc3a15
deanrobin333 <deanrobin777@gmail.com>
2023-12-09 10:03:29 +0300

    100-rot13.c changed to 2 loops and 1 if statement

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,27 +5,32 @@
  * @str: pointer to string to encode
  *
  * Return: pointer of encoded string
+ *
+ * break statement is used to exit the inner loop immediately after
+ *		the first replacement is done.
+ * Without the break statement, if a replacement is made for a character,
+ *		the inner loop would continue to iterate through the
+ *		remaining characters in the letters array even after a match is found
+ *		and a replacement is made;
+ *		potentially causing additional unintended replacements
+ *		for the same character.
  */
 
 char *rot13(char *str)
 {
-	int i;
+	int i, j;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotated[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		for (j = 0; j < 52; j++)
 		{
-			if (str[i] >= 'a' && str[i] <= 'm')
-				str[i] = str[i] + 13;
-			else if (str[i] >= 'n' && str[i] <= 'z')
-				str[i] = str[i] - 13;
-		}
-		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			if (str[i] >= 'A' && str[i] <= 'M')
-				str[i] = str[i] + 13;
-			else if (str[i] >= 'N' && str[i] <= 'Z')
-				str[i] = str[i] - 13;
+			if (str[i] == letters[j])
+			{
+				str[i] = rotated[j];
+				break;
+			}
 		}
 	}
 	return (str);
