5a7958c75e756ffdd4dfbdd79e68fccf6b86ff2b
deanrobin333 <deanrobin777@gmail.com>
2023-12-09 05:06:00 +0300

    7-leet.c - updated comments

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,6 +4,12 @@
  * leet - encodes a string into 1337
  * @str: pointer to string
  *
+ * Letters `a` and `A` should be replaced by `4`
+ * Letters `e` and `E` should be replaced by `3`
+ * Letters `o` and `O` should be replaced by `0`
+ * Letters `t` and `T` should be replaced by `7`
+ * Letters `l` and `L` should be replaced by `1`
+ *
  * Return: pointer to encoded string
  */
 
