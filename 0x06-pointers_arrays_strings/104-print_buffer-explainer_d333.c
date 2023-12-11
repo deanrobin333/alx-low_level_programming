5ba6350356880391a693bbfcabc1e18a6a972930
deanrobin333 <deanrobin777@gmail.com>
2023-12-11 08:15:43 +0300

    104-print_buffer.c - moved comments to top

--- 0x06-pointers_arrays_strings/104-print_buffer-explainer_d333.c
+++ 0x06-pointers_arrays_strings/104-print_buffer-explainer_d333.c
@@ -36,70 +36,7 @@
  * This goes on until bytes remaining are less than ten `size - bytes`
  * if remaining bytes is less than ten, then it is set to that number
  *		which is `size - bytes`
- *
- *
- *
- * `if (size - bytes < 10)`
- *
- * print the (bytes % 10 = 0) byte no. in hexadecimal
- * bytes will always increase by 10 until
- * `size - bytes` is less than ten
- * when you `08` after `%`, we are creating a padding of 8 zeros
- *
- *
- *
- *
- *
- * `printing = i + bytes;`
- *
- * `printing` - we add bytes to i, since print the first ten bytes,
- * then the next ten bytes.
- * `bytes` increases by ten
- * so we need to print the `i` + 10 bytes.
- *
- *
- *
- *
- *
- * `for ( ; bytes < size; bytes += 10)`
- *
- * we check if bytes to print are less than total size (total bytes).
- * if they are not less we set `first_ten` to `10`
- * if they are less, it means we are on the remaining bytes,
- * so we set `first_ten` to `size - bytes`
- *
- * `i` will always be compared to `first_ten`
- * but we will be printing `i + bytes`
- *	because we are printing in batches of 10.
-* note: bytes is increased by 10, after each print.
-*
-*
-*
-*
-* `printf("%02x", b[printing]);`
-*
-* `%02x` ensures if the hexadecimal has one digit, 0 is added
-* so that it always has 2 digits
-* we now print the hexadecimal of each printing character in twos
-* we print the first 10, so ten hexadecimal values
-*
-* we do `if (i < first_ten)` so that it will keep printing,
-* until `i` is greater than `first_ten`.
-* `first_ten` will always be 10, until we reach the last bytes
-*		not more than ten
-* then `first_ten` becomes less than `i` ie `size - bytes`
-*
-* `i` iterates by 1 until 10.
-*		for as long as i is less than `first_ten` it prints.
-* when we reach the last (< 10) bytes, `first_ten`
-*		will decrease until `i` becomes greater then `first_ten`
-*		and then printing stops
-*
-* we are just printing the first ten bytes in hexadecimal
-*		not the hexadecimal values of the printable characters.
-* if we needed the hexadecimal representation only, we would
-*		have: `if (b[printing] >=32 && b[printing] <=126)`
-*/
+ */
 
 void print_buffer(char *b, int size)
 {
@@ -111,6 +48,17 @@ void print_buffer(char *b, int size)
 		return;
 	}
 
+	/**
+	 * we check if bytes to print are less than total size (total bytes).
+	 * if they are not less we set `first_ten` to `10`
+	 * if they are less, it means we are on the remaining bytes,
+	 * so we set `first_ten` to `size - bytes`
+	 *
+	 * `i` will always be compared to `first_ten`
+	 * but we will be printing `i + bytes`
+	 *	because we are printing in batches of 10.
+	 * note: bytes is increased by 10, after each print.
+	 */
 	for ( ; bytes < size; bytes += 10)
 	{
 		if (size - bytes < 10)
@@ -118,10 +66,48 @@ void print_buffer(char *b, int size)
 		else
 			first_ten = 10;
 
+		/**
+		 * print the (bytes % 10 = 0) byte no. in hexadecimal
+		 * bytes will always increase by 10 until
+		 * `size - bytes` is less than ten
+		 * when you `08` after `%`, we are creating a padding of 8 zeros
+		 */
 		printf("%08x: ", bytes);
+
+		/**
+		 * we now print the hexadecimal of each printing character in twos
+		 * we print the first 10, so ten hexadecimal values
+		 *
+		 * we do `if (i < first_ten)` so that it will keep printing,
+		 * until `i` is greater than `first_ten`.
+		 * `first_ten` will always be 10, until we reach the last bytes
+		 *		not more than ten
+		 * then `first_ten` becomes less than `i` ie `size - bytes`
+		 *
+		 * `i` iterates by 1 until 10.
+		 *		for as long as i is less than `first_ten` it prints.
+		 * when we reach the last (< 10) bytes, `first_ten`
+		 *		will decrease until `i` becomes greater then `first_ten`
+		 *		and then printing stops
+		 *
+		 * we are just printing the first ten bytes in hexadecimal
+		 *		not the hexadecimal values of the printable characters.
+		 * if we needed the hexadecimal representation only, we would
+		 *		have: `if (b[printing] >=32 && b[printing] <=126)`
+		 */
 
 		for (i = 0; i < 10; i++)
 		{
+			/**
+			 * `printing` - we add bytes to i, since print the first ten bytes,
+			 * then the next ten bytes.
+			 * `bytes` increases by ten
+			 * so we need to print the `i` + 10 bytes.
+			 */
+			/*
+			 * `%02x` ensures if the hexadecimal has one digit, 0 is added
+			 * so that it always has 2 digits
+			 */
 			printing = i + bytes;
 			if (i < first_ten)
 				printf("%02x", b[printing]);
