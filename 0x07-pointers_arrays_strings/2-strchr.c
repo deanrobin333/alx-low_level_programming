2956fd2426d61ad9a42784906d74fb8d057ca706
deanrobin333 <deanrobin777@gmail.com>
2023-12-12 17:25:40 +0300

    2-strchr.c - updated comments

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,6 +9,14 @@
  *
  * Return: pointer to character found
  *			NULL if the character is not found
+ *
+ *
+ * We used a loop to find the size of s.
+ * so that we use `len` in the 2nd loop instead of `s[i] != '\0'`
+ * the loop gives us the number of elements,
+ *		hence if elements are 5, the last element is on the 4th index.
+ * but we iterate up to the 5th index, so that we include the null character
+ *		`c` might be a null character, and if so, we still print it.
  */
 
 char *_strchr(char *s, char c)
