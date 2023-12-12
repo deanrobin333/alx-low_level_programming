234e4a9faedb6e723b7e40edb84afe111b6a02e8
deanrobin333 <deanrobin777@gmail.com>
2023-12-12 17:13:19 +0300

    2-strchr.c - account if c is a null character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,11 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 	char *found = NULL;
+	unsigned int len = sizeof(s);
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (s[i] == c)
 		{
