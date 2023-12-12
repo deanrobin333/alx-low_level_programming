b0b919d4d161616f548aab7228bb12bc86ca5e1a
deanrobin333 <deanrobin777@gmail.com>
2023-12-12 18:11:57 +0300

    3-strspn.c updated comments

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,6 +6,7 @@
  * @accept: string to check if bytes are present in @s
  *
  * Return: pointer to found bytes
+ *
  */
 
 unsigned int _strspn(char *s, char *accept)
@@ -16,12 +17,21 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
+			/**
+			 * If a character in s matches any character in accept,
+			 * the bytes counter is incremented by 1, and the inner loop is
+			 * broken using break.
+			 */
 			if (s[i] == accept[j])
 			{
 				bytes += 1;
 				break;
 			}
 		}
+		/**
+		 * The second if statement ensures that the function stops searching if
+		 *		a character is encountered in `s` that is not present in `accept`
+		 */
 		if (s[i] != accept[j])
 			break;
 	}
