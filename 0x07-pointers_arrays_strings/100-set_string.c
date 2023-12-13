6d83928936b6b3db0de7dd688932a442dd8afd62
deanrobin333 <deanrobin777@gmail.com>
2023-12-13 13:01:37 +0300

    100-set_string.c - updated comments

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -6,6 +6,21 @@
  * @to: pointer to string to change
  *
  * Return: void
+ *
+ * We have 2 strings
+ *		char *s0 = "Bob Dylan";
+ *		char *s1 = "Robert Allen";
+ * The function is called like so:
+ *		set_string(&s1, s0);
+ *
+ * Since `s1` is a pointer, to pass it to a function, it must be passed
+ *		to a double pointer.
+ * so `**s` will point to the address of `*s1`, and
+ *		we dereference the value of `*s1` from `**s` by `*s`
+ *
+ * `*to` points to `*s0`.
+ * so we change the value of `*s0`, by setting `to` to `*s`
+ *
  */
 
 void set_string(char **s, char *to)
