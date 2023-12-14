e7d56bc73c0b5176d15ce33d9e0fdf84ea91c323
deanrobin333 <deanrobin777@gmail.com>
2023-12-14 10:58:05 +0300

    1-print_rev_recursion.c - updated comments

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,6 +6,16 @@
  * @s: pointer to string to print
  *
  * Return: void
+ *
+ * Position of recursive call matters.
+ * before we pring using _putchar, the recursive call is done
+ *		until it hits the null character in the string.
+ *		when it hits, it starts to return.
+ *
+ * Recursive function uses a stack that operates in last in first out.
+ * When the recursive call hits the '\0' character, it returns the
+ *		characters to _putchar, starting from the last one (LIFO), to the first.
+ * Hence priting in reverse.
  */
 
 void _print_rev_recursion(char *s)
