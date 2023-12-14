7809a64f6af68da80629415b82fdb8813559305d
deanrobin333 <deanrobin777@gmail.com>
2023-12-14 14:17:35 +0300

    3-factorial.c - updated comments

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,6 +5,19 @@
  * @n: number to calculate factorial
  *
  * Return: the factorial
+ *
+ *
+ * the factorial of a non-negative integer is the product of all
+ *		positive integers less than or equal to that number.
+ *
+ * factorial of 3 is 3 * 2 * 1.
+ * factorial of 5 is 5 * 4 * 3 * 2 * 1
+ *
+ * so bascally the factorial of `n` is `n` * the factorial of `n` - 1.
+ * n! = n * (n -1)!
+ *
+ * The factorial of 0 is defined to be 1, by convention:
+ *		this then can make our base case to stop recursion.
  */
 
 int factorial(int n)
