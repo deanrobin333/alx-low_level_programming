e950c617e2b20eb07e70a089ab8833f9f8ed783f
deanrobin333 <deanrobin777@gmail.com>
2023-12-14 10:06:38 +0300

    0-puts_recursion.c - updated comments

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,20 @@
  * @s: pointer to string to print
  *
  * Return: void
+ *
+ * We first print the 0th indexed character
+ * the reursive call `_puts_recursion(s + 1)` moves the pointer
+ *		to the next character in the string, making the next character
+ *		the 0th index; we then print it.
+ *
+ * when we reach the null character, we print a new line, and return to `main`.
+ *		this successfully sets our base case.
+ * the base case in recursion is what terminates the recursive call.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
