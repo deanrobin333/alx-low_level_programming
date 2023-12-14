013551b1fe2f9cccc856e32e453624bff6493cac
deanrobin333 <deanrobin777@gmail.com>
2023-12-14 13:55:36 +0300

    2-strlen_recursion.c updated comments

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,6 +5,13 @@
  * @s: pointer to string
  *
  * Return: length of string
+ *
+ * since each recursive call creates a new instance of a function,
+ *		every time the recursive call is called len = 0.
+ * so if we use `len++`, len will be increased by 1, once.
+ * so if we want to increase len for each recursive call, we must set `len` to
+ *		`1 + recursive call`, so that when the recursive call reaches the end,
+ *		and it begins to return, for every character it returns, it adds 1 to len.
  */
 
 int _strlen_recursion(char *s)
