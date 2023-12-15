be39d9de37e5b1fcbf9e995030fd7a64bfa542b6
deanrobin333 <deanrobin777@gmail.com>
2023-12-15 18:14:19 +0300

    4-add.c removed 2nd function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int check_digit(char *str);
 
 /**
  * main - adds positive numbers
@@ -14,40 +13,27 @@ int check_digit(char *str);
 
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc <= 1)
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		if (!check_digit(argv[i]))
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			/* loop through the arguments */
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
 		}
-		sum = sum + atoi(argv[i]);
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
-	return (0);
-}
-
-/**
- * check_digit - helper function that checks if a character is a digit
- * @str: pointer to string, which is `argv`
- *
- * Return: 1 (for true) if character is a string
- *			0 (for false) if character is not a string
- */
-
-int check_digit(char *str)
-{
-	if (isdigit(*str))
-		return (1);
+	else
+		printf("0\n");
 
 	return (0);
 }
-
