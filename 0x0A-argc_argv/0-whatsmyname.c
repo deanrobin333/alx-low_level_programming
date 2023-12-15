3f25d5d81a17fbb0b6dd08ef5bfabb5c8325964e
deanrobin333 <deanrobin777@gmail.com>
2023-12-15 16:01:40 +0300

    0-whatsmyname.c - betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,13 @@
 
 /**
  * main - program that prints its name followed by a new line
+ * @argc: command line arguments count
+ * @argv: pointer to command line arguments
  *
  * Return: EXIT_SUCCESS
  */
 
-int main(__attribute__((unused))int argc, char ** argv)
+int main(__attribute__((unused))int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 
