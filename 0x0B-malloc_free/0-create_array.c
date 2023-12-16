cea298469b3fe0e4ce12b3ab0b92ef8a3c880d20
deanrobin333 <deanrobin777@gmail.com>
2023-12-16 15:19:28 +0300

    0-create_array.c - if size is 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,9 @@ char *create_array(unsigned int size, char c)
 	char *array = NULL;
 	unsigned int i;
 
+	if (size <= 0)
+		return (NULL);
+
 	array = (char *)malloc(size);
 
 	for (i = 0; i < size; i++)
