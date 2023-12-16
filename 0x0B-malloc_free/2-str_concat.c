469f248a52716fda2d3e055cd8132a085abfe9cf
deanrobin333 <deanrobin777@gmail.com>
2023-12-16 18:17:55 +0300

    2-str_concat.c - wrapped loop in else

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,14 +30,17 @@ char *str_concat(char *s1, char *s2)
 
 	if (array == NULL)
 		return (NULL);
+	else
+	{
 
 	for (i = 0; s1[i] != '\0'; i++)
-		array[i] += s1[i];
+		array[i] = s1[i];
 
 	/* using strlen, so that it gets the right length */
 
 	for (k = 0; s2[k] != '\0'; k++)
-		array[strlen(array)] += s2[k];
+		array[strlen(array)] = s2[k];
+	}
 
 	array[strlen(array)] = '\0';
 
