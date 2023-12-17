53d5888a3d543ed35278027dc48a534a03dcdb23
deanrobin333 <deanrobin777@gmail.com>
2023-12-17 13:02:00 +0300

    3-array_range.c - betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	
+
 	array = malloc(sizeof(int) * (max - min + 1));
 
 	if (array == NULL)
@@ -23,7 +23,7 @@ int *array_range(int min, int max)
 
 	for (i = min, k = 0; i <= max; i++, k++)
 	{
-		array[k] = min++;
+		array[k] = i;
 	}
 
 	return (array);
