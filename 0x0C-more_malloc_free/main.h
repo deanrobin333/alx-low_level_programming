88837c1a72fa10a87ec376e5da1831042be83c1a
deanrobin333 <deanrobin777@gmail.com>
2023-12-17 13:18:57 +0300

    advanced task files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -15,5 +15,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 /* 3-array_range.c */
 int *array_range(int min, int max);
 
+/* 100-realloc.c */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* 101-mul.c */
 
 #endif /*_MAIN_H_ */
