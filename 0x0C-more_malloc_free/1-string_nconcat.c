d9af7d779a8c3809e1dd2f969245c96e9fe082f7
deanrobin333 <deanrobin777@gmail.com>
2023-12-17 11:07:50 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		array[strlen(array)] = s2[i];
+		array[i + strlen(s1)] = s2[i];
 	}
 
 	array[strlen(array)] = '\0';
