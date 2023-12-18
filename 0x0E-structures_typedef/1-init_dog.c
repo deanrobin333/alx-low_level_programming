60cdbed6c2a635a9a4490553c3b18a181dc1b912
deanrobin333 <deanrobin777@gmail.com>
2023-12-18 11:52:13 +0300

    1-init_dog.c - missed bracket in malloc

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -18,7 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	 * and give it the type struct dog
 	 */
 	if (d == NULL)
-		d = malloc(sizeof(struct dog);
+		d = malloc(sizeof(struct dog));
 
 	d->name = name;
 	d->age = age;
