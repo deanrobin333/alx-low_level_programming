efdf85b1c10a183e064fd4488329d536b42b6a5b
deanrobin333 <deanrobin777@gmail.com>
2023-12-18 11:50:56 +0300

    1-init_dog.c - check if d is null

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,6 +12,14 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	/**
+	 * check if d has not been assigned, if it is null
+	 * if it is null, we assign it using malloc,
+	 * and give it the type struct dog
+	 */
+	if (d == NULL)
+		d = malloc(sizeof(struct dog);
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
