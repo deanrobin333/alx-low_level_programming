8f81db437110d4f89efead910377d904f94dbb62
deanrobin333 <deanrobin777@gmail.com>
2023-12-22 14:28:24 +0300

    1-print_numbers.c - removed check for n <= 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,8 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list values;
 
-	if (n <= 0)
-		return;
 	va_start(values, n);
 
 	for (i = 0; i < n; i++)
