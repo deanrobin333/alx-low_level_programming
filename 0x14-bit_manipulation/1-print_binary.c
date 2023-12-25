217e6f7b4aae34e2129b01f7339e53e0e6f0ebd4
deanrobin333 <deanrobin777@gmail.com>
2023-12-26 00:17:07 +0300

    1-print_binary.c - updated comments

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,8 +10,16 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int max_int = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	int start_printing = 0;
+	/**
+	 * This line initializes max_int with the most significant bit (MSB)
+	 *		set to 1 and all other bits set to 0.
+	 * The expression `1UL << (sizeof(unsigned long int) * 8 - 1)` uses
+	 *		bit-shifting to set the MSB of an unsigned long int.
+	 * UL - sets it to an unsigned long.
+	 * same to what we would for int type - `max_int = 1 << 31`.
+	 */
+	 unsigned long int max_int = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	 int start_printing = 0;
 
 	if (n == 0)
 	{
@@ -21,15 +29,26 @@ void print_binary(unsigned long int n)
 
 	for (; max_int > 0; max_int >>= 1)
 	{
+		/**
+		 * checks if the set bit in the current position (being checked)
+		 *		of `max_int` is also set in the number n.
+		 * If it is, then we know that the binary representation of n has a 1
+		 *		in that position, and we proceed to print 1.
+		 *		If it isn't, then we print 0.
+		 */
 		if (n & max_int)
 		{
 			start_printing = 1;
 			_putchar('1');
 		}
+		/**
+		 * the start_printing flag and the max_int == 1 condition work together
+		 *		to ensure that the binary representation of n is
+		 *		printed correctly without any leading zeros and
+		 *		with all the necessary bits.
+		 */
 		else if (start_printing || max_int == 1)
-		{
 			_putchar('0');
-		}
 	}
 }
 
