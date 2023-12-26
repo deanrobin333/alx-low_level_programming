76ad75fea29c2de7e9d24fcb7115ba63b8b036c2
deanrobin333 <deanrobin777@gmail.com>
2023-12-26 03:04:59 +0300

    1-print_binary.c - using loop; most preferred option than previous

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,47 +8,41 @@
  * Return: void
  */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+
+
 void print_binary(unsigned long int n)
 {
-	/**
-	 * This line initializes max_int with the most significant bit (MSB)
-	 *		set to 1 and all other bits set to 0.
-	 * The expression `1UL << (sizeof(unsigned long int) * 8 - 1)` uses
-	 *		bit-shifting to set the MSB of an unsigned long int.
-	 * UL - sets it to an unsigned long.
-	 * same to what we would for int type - `max_int = 1 << 31`.
-	 */
-	 unsigned long int max_int = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	 int start_printing = 0;
+	unsigned long int m, present;
+	/* must set i to int, as if it unsigned, it cant be less than 0*/
+	int count = 0, i;
 
-	if (n == 0)
+	m = sizeof(unsigned long int) * 8 - 1;
+	for (i = m; i >= 0; i--)
 	{
-		_putchar('0');
-		return;
-	}
-
-	for (; max_int > 0; max_int >>= 1)
-	{
-		/**
-		 * checks if the set bit in the current position (being checked)
-		 *		of `max_int` is also set in the number n.
-		 * If it is, then we know that the binary representation of n has a 1
-		 *		in that position, and we proceed to print 1.
-		 *		If it isn't, then we print 0.
-		 */
-		if (n & max_int)
+		present = n >> i;
+		if (present & 1)
 		{
-			start_printing = 1;
 			_putchar('1');
+			count++;
+		}
+		else if (count != 0)
+		{
+			/**
+			 * we keep skiping leading 0.
+			 * if 1 is printed count is no longer 0, hence print leading 0s.
+			 */
+			_putchar('0');
 		}
 		/**
-		 * the start_printing flag and the max_int == 1 condition work together
-		 *		to ensure that the binary representation of n is
-		 *		printed correctly without any leading zeros and
-		 *		with all the necessary bits.
+		 * if no `1` bit is met, it means the last bit is `0`
+		 * so we make sure we handle this scenario
 		 */
-		else if (start_printing || max_int == 1)
-			_putchar('0');
+		if (count == 0)
+			_putchar(0);
+
 	}
 }
 
