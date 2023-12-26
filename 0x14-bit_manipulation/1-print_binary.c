1f13db5edacea86204ce57e27c406c8796a45178
deanrobin333 <deanrobin777@gmail.com>
2023-12-26 03:31:21 +0300

    1-print_binary.c - using loop; most preferred option than previous -2 - clean up

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
+#include <limits.h>
 /**
  * print_binary - prints binary representation of a number
  * @n: number to print
  *
  * Return: void
  */
-
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
-
 
 void print_binary(unsigned long int n)
 {
