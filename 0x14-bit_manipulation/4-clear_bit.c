a5b2330622d4435db4d986caa2d9557902b14c87
deanrobin333 <deanrobin777@gmail.com>
2023-12-26 20:07:17 +0300

    4-clear_bit.c - flipped  to

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,10 +12,12 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8 - 1)
 		return (-1);
-	if (*n == 0)
-		return (0);
 
-	*n = ((1UL << index) ^ *n);
+	/**
+	 * `~` - we move `1` to indexth bit, then we flip it so that it is 0.
+	 * `&` - then AND with n. will always be `0`, so indexth bit of n wil be 0.
+	 */
+	*n = (~(1UL << index) & *n);
 
 	return (1);
 }
