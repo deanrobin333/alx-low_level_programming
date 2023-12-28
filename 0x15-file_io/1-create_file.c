a31bf0e84cdd05ffc08969db6a8ad27ad855cf8a
deanrobin333 <deanrobin777@gmail.com>
2023-12-28 16:08:53 +0300

    1-create_file.c -create empty file if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,17 +17,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes_written;
+	ssize_t bytes_written, len;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+		len = strlen(text_content);
+	else
+		len = 0;
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	bytes_written = write(fd, text_content, strlen(text_content));
+	bytes_written = write(fd, text_content, len);
 	if (bytes_written == -1)
 		return (-1);
 
