381b390558400e0b90702b07dd80f78c3fc86325
deanrobin333 <deanrobin777@gmail.com>
2023-12-28 16:25:57 +0300

    1-create_file.c -minor updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	else
 		len = 0;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
