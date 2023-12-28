5958916469966e8e1959507bad52196af387796c
deanrobin333 <deanrobin777@gmail.com>
2023-12-28 16:00:13 +0300

    1-create_file.c - always close the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -30,6 +30,8 @@ int create_file(const char *filename, char *text_content)
 	bytes_written = write(fd, text_content, strlen(text_content));
 	if (bytes_written == -1)
 		return (-1);
+
+	close(fd); /*always remember to close */
 
 	return (1);
 }
