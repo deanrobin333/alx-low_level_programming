4a5cf519c6c344078626d8254ae7b8159324d8c0
deanrobin333 <deanrobin777@gmail.com>
2024-01-17 10:54:17 +0300

    0x1E-search_algorithms

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -25,4 +25,7 @@ int clear_bit(unsigned long int *n, unsigned int index);
 /* 5-flip_bits.c */
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
+/* 100-get_endianness.c */
+int get_endianness(void);
+
 #endif /* _MAIN_H_ */
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,2 @@
+# Project 0x1E. C - Search Algorithms
+Project started on 9th Oct
