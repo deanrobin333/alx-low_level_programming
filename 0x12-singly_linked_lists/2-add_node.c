eb82e6f096b1121dde0811406ec00384128d0d32
deanrobin333 <deanrobin777@gmail.com>
2024-02-09 16:58:28 +0300

    2-add_node.c - created unsinged int for length

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,15 +12,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int length;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
+	length = strlen(str);
+
 	new->str = strdup(str);
-	new->len = strlen(new->str);
+	new->len = length;
 	new->next = *head;
 	*head = new;
 
-	return (*head);
+	return (new);
 }
