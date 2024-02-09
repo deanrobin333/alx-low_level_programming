0beade813882668fdfbcab2ab100738755411131
deanrobin333 <deanrobin777@gmail.com>
2024-02-09 16:18:56 +0300

    0-print_list.c - betty for lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,9 +13,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 /* 0-print_list.c */
