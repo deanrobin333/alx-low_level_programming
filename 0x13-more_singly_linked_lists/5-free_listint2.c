3ccf3a2c2caff94c48126a79d62def6577d0a868
deanrobin333 <deanrobin777@gmail.com>
2024-02-09 21:42:11 +0300

    5-free_listint2.c - betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,7 +4,7 @@
  * free_listint2 - frees as listint_t list and sets head to NULL
  * @head: double pointer to list; first node.
  *
- * @Return: void
+ * Return: void
  */
 
 void free_listint2(listint_t **head)
