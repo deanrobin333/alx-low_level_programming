d636fe78376e057df12542c2cafb1b5c98e30d18
deanrobin333 <deanrobin777@gmail.com>
2024-02-10 00:22:03 +0300

    9-insert_nodeint.c - handled if list is empty and if idx is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,13 +16,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
 
 	new->n = n;
 
-	if ((*head)->next == NULL)
-		new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
 	current = *head;
 	while (i < idx)
