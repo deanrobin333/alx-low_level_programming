4445b4ff1403f841c0896029337ae50287f5cce3
deanrobin333 <deanrobin777@gmail.com>
2024-02-10 03:47:33 +0300

    100-reverse_listint.c condensed the code - 3

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -17,35 +17,40 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *next, *previous;
 
 	/**
-	 * if linked list is empty or first node is NULL
-	 * While loop will not be triggered
-	 * it will return *head which is NULL
+	 * Initialize both to NULL, so that if list is empty or head is NULL
+	 * it will return head, which will still be NULL
 	 */
-	if (head == NULL || *head == NULL)
-		return (NULL);
-
-	next = (*head)->next;
+	next = NULL;
 	previous = NULL;
 
-	while (next != NULL)
+	while (*head != NULL)
 	{
+		/* if list isn't empty, we can now initialize next */
+		next = (*head)->next;
+
 		/* point next node of *head to the reverse */
 		(*head)->next = previous;
 
 		/* shift the positions of the nodes foward */
 		previous = *(head);
 		*head = next;
-		next = next->next;
 
 		/**
 		 * when next is NULL, we have reached the end of list
-		 * so we terminate the loop
+		 * so we terminate the loop.
+		 *
+		 * and hence since at the end, head == next,
+		 * we can check for either *head or next if it is NULL
+		 *
+		 * head will be the next pointer of the last node
 		 */
 	}
 	/**
-	 * (*head) is currently the last node
-	 * so we make next pointer of (*head) reverse, to point to the node before
+	 * (*head) is currently the next pointer of last node
+	 * so we make (*head) the last node, which is now the first node in reverse
+	 *
+	 * Also if empty list, loop isn't triggered and previous remains NULL.
 	 */
-	(*head)->next = previous;
+	(*head) = previous;
 	return ((*head));
 }
